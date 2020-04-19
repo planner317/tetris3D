5f08ca730f8d4d669264f4ab0bf80c0fc7f2a450
planner317 <planner317@gmail.com>
2020-04-19 17:14:25 +0300

    fix

--- js/texture317.js
+++ js/texture317.js
@@ -3,65 +3,65 @@ var texture317 = new Texture317();
 
 function Texture317() {
 
-    new THREE.TextureLoader().load("../img/SPONGE.JPG",
+    new THREE.TextureLoader().load("/img/SPONGE.JPG",
         (texture) => {
             this.sponge = texture;
         }
     );
     //////////////
-    new THREE.TextureLoader().load("../img/wood.jpg",
+    new THREE.TextureLoader().load("/img/wood.jpg",
         (texture) => {
             this.wood = texture;
         }
     );
     ///////////////
-    new THREE.TextureLoader().load("../img/MtlPlat2.jpg",
+    new THREE.TextureLoader().load("/img/MtlPlat2.jpg",
         (texture) => {
             this.metal2 = texture;
         }
     );
-    new THREE.TextureLoader().load("../img/MtlPlat2_Bump.jpg",
+    new THREE.TextureLoader().load("/img/MtlPlat2_Bump.jpg",
         (texture) => {
             this.metal2Bump = texture;
         }
     );
     //////////////
-    new THREE.TextureLoader().load("../img/Loose.jpg",
+    new THREE.TextureLoader().load("/img/Loose.jpg",
         (texture) => {
             this.loose = texture;
         }
     );
-    new THREE.TextureLoader().load("../img/Loose.Bump.jpg",
+    new THREE.TextureLoader().load("/img/Loose.Bump.jpg",
         (texture) => {
             this.looseBump = texture;
         }
     );
-    new THREE.TextureLoader().load("../img/Loose.Mask.jpg",
+    new THREE.TextureLoader().load("/img/Loose.Mask.jpg",
         (texture) => {
             this.looseAlfa = texture;
             console.log('Loose.Mask.jpg');
 
         });
     //////////////////
-    new THREE.TextureLoader().load("../img/stone.jpg",
+    new THREE.TextureLoader().load("/img/stone.jpg",
         (texture) => {
             this.stone = texture;
         }
     );
     ////////////////////////////
-    new THREE.TextureLoader().load("../img/lavatile.jpg",
+    new THREE.TextureLoader().load("/img/lavatile.jpg",
         (texture) => {
             this.lava = texture;
         }
     );
     ////////////////////////////
-    new THREE.TextureLoader().load("../img/FloorsNormal.jpg",
+    new THREE.TextureLoader().load("/img/FloorsNormal.jpg",
         (texture) => {
             this.floorNormal = texture;
         }
     );
     ////////////////////////////
-    new THREE.TextureLoader().load("../img/portal_360.jpg",
+    new THREE.TextureLoader().load("/img/portal_360.jpg",
         (texture) => {
             texture.mapping = THREE.EquirectangularReflectionMapping;
 
@@ -70,12 +70,12 @@ function Texture317() {
         }
     );
     ////////////////////////////
-    new THREE.TextureLoader().load("../model/cube_heart.jpg",
+    new THREE.TextureLoader().load("/model/cube_heart.jpg",
         (texture) => {
             this.portalCubeD = texture;
         }
     );
-    new THREE.TextureLoader().load("../model/cube_heart_light.jpg",
+    new THREE.TextureLoader().load("/model/cube_heart_light.jpg",
     (texture) => {
         this.portalCubeL = texture;
     }
